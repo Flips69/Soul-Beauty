4f9b069d8d75fd0a68f715d48b8fd99b39d7544c
Flips69 <aion.lipe@gmail.com>
2023-10-03 15:34:07 -0300

    Commit do começo da agenda

---
+++ app/Http/Controllers/AgendaController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AgendaFormRequest;
+use App\Models\Agenda;
+use Illuminate\Http\Request;
+
+class AgendaController extends Controller
+{
+    public function store(AgendaFormRequest $request){
+        $agenda = Agenda::create([
+            'profissional_id' => $request->profissional_id,
+            'cliente_id' => $request->cliente_id,
+            'servico_id' => $request->servico_id,
+            'data_hora' => $request->data_hora,
+            'tipo_pagamento' => $request->tipo_pagamento,
+            'valor' => $request->valor,
+        ]);
+    }
+}
